32cd3d84ffeec78555b882add28e2b0a6bbcdc7d
Makar <makaresh123@yandex.ru>
2021-02-07 21:48:05 +0400

    Initial commit

---
+++ src/test/java/makar/ru/currency/CurrencyConverterApplicationTests.java
@@ -0,0 +1,13 @@
+package makar.ru.currency;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyConverterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
